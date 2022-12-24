a9e713bde6bccb09f9d9f1b596f7f811f103a873
bruno.silva <bruno.sergio.165@gmail.com>
2022-12-24 14:43:47 -0300

    classe user

---
+++ src/main/java/br/com/czo/api2/domain/User.java
@@ -0,0 +1,23 @@
+package br.com.czo.api2.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "TB_USER")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
