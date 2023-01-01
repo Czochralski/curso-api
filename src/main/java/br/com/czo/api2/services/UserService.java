86fa610905429edb4565aef774fb715c19a999f0
bruno.silva <bruno.sergio.165@gmail.com>
2023-01-01 12:01:49 -0300

    end point findAll para user

--- src/main/java/br/com/czo/api2/services/UserService.java
+++ src/main/java/br/com/czo/api2/services/UserService.java
@@ -2,6 +2,9 @@ package br.com.czo.api2.services;
 
 import br.com.czo.api2.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User findById(Integer id);
+    List<User> findAll();
 }
