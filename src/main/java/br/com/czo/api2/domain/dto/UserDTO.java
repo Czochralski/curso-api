d641931d27cf33d20b25057d7e1db039658b158c
bruno.silva <bruno.sergio.165@gmail.com>
2023-01-01 11:22:11 -0300

    classe user dto

---
+++ src/main/java/br/com/czo/api2/domain/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package br.com.czo.api2.domain.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
