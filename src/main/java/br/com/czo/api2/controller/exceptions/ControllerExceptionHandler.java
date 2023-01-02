09338e1163a560b75f9043618c006addab8c9b0e
bruno.silva <bruno.sergio.165@gmail.com>
2023-01-02 17:22:56 -0300

    teste para manipulador de exceções metodo metodo data integrity

--- src/main/java/br/com/czo/api2/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/br/com/czo/api2/controller/exceptions/ControllerExceptionHandler.java
@@ -1,8 +1,9 @@
 package br.com.czo.api2.controller.exceptions;
 
-import br.com.czo.api2.services.exceptions.DataIntegratyViolationException;
+
 import br.com.czo.api2.services.exceptions.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,8 +21,8 @@ public class ControllerExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
-    @ExceptionHandler(DataIntegratyViolationException.class)
-    public ResponseEntity<StandardError>dataIntegratyViolationException(DataIntegratyViolationException ex, HttpServletRequest request) {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError>dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request) {
         StandardError error =
                 new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
                 ex.getMessage(), request.getRequestURI());
--- src/test/java/br/com/czo/api2/controller/exceptions/ControllerExceptionHandlerTest.java
+++ src/test/java/br/com/czo/api2/controller/exceptions/ControllerExceptionHandlerTest.java
@@ -6,9 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -26,6 +29,7 @@ class ControllerExceptionHandlerTest {
         ResponseEntity<StandardError> response = exceptionHandleron
                 .objectNotFound(new ObjectNotFoundException("Objeto não encontrado"),
                                  new MockHttpServletRequest());
+
         assertNotNull(response);
         assertNotNull(response.getBody());
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
@@ -33,9 +37,21 @@ class ControllerExceptionHandlerTest {
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals("Objeto não encontrado", response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+        assertNotEquals("/user/2",response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(),response.getBody().getTimestamp());
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void whenDataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandleron
+                .dataIntegrityViolationException(new DataIntegrityViolationException("Email já cadastrado!"),
+                        new MockHttpServletRequest());
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("Email já cadastrado!", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
